c894a68b0ed04150166dfa1f611e422672300cd3
Nickolay Trukhin <coolone.official@gmail.com>
2021-10-31 09:41:03 +0300

    Initial Commit

---
+++ mad_autumn_2021/mad_autumn_2021App.swift
@@ -0,0 +1,17 @@
+//
+//  mad_autumn_2021App.swift
+//  mad_autumn_2021
+//
+//  Created by Nickolay Truhin on 31.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct mad_autumn_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
